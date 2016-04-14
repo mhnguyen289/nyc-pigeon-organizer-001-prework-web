27423ce9eaac30c1b4a4664420dba72aecb757c0
mhnguyen289 <mhnguyen289@gmail.com>
2016-04-14 12:42:16 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  all_names = []
+  pigeon_list = {}
+
+  data.each do |key, value|
+    value.each do |k,v|
+      v.each do |name|
+        all_names.push(name.to_s)
+      end
+    end
+  end
+  all_names.uniq!
+all_names.each do |x|
+  pigeon_list[x] = {}
+  pigeon_list[x][:color] = []
+  pigeon_list[x][:gender] = []
+  pigeon_list[x][:lives] = []
+  #:color, :gender, :lives
+end
+
+data.each do |x,y|
+  y.each do |key, value|
+    value.each do |name|
+      all_names.each do |pigeon|
+      if name == pigeon && pigeon_list[pigeon][x]
+        pigeon_list[pigeon][x].push(key.to_s)
+      end
+      end
+    end
+  end
+end
+pigeon_list
 end
